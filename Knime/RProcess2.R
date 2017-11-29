d477e332cd57d6c0a45075cf3841a1105d2215bd
Nathaniel Twarog <nathanieltwarog@users.noreply.github.com>
2017-11-28 23:16:42 -0600

    Create RProcess2.R

---
+++ Knime/RProcess2.R
@@ -0,0 +1,8 @@
+# Node 12: Table to R
+# Read in raw data that has been selected for refitting by user
+rawdata <- knime.in
+
+# Node 13: Add Table to R
+# Read in upper and lower temperature bounds provided by user
+welldata <- knime.in
+
